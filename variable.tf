806afdd5ac1ba2838491f19a0bd0dd544cd5f625
Malaroy123 <sqrt0fmal@gmail.com>
2025-03-30 17:26:17 +0100

    create ACM file

--- variable.tf
+++ variable.tf
@@ -77,3 +77,14 @@ variable "multi_az_deployment" {
   description = "create a standby db instance"
   type        = bool
 }
+
+# ACM variables
+variable "domain_name" {
+  description = "domain name"
+  type        = string
+}
+
+variable "alternative_names" {
+  description = "sub domain name"
+  type        = string
+}
